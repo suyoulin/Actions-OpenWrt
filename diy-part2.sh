89530b0c06211ca683e78d0c4add25713f5cb09c
suyoulin <168067232+suyoulin@users.noreply.github.com>
2024-05-08 15:18:47 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,5 +21,5 @@ sed -i 's/luci-theme-bootstrap/luci-theme-openwrt-2020/g' feeds/luci/collections
 
 # Open wifi
 # sed -i 's/36/44/g;s/VHT80/HE20/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
-# sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
